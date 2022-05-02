aed7031bde61174684f1fafbaf5633f0b8f38ba3
Yelle <lieder@mailbox.org>
2022-05-02 12:59:44 +0200

    added config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+#https://www.deepl.com/en/docs-api/
+deepl_api_key = ""
+#https://openai.com/api/
+openai_api_key = ""
+#bot output language as in https://www.deepl.com/docs-api/other-functions/listing-supported-languages/
+target_language="DE"
+#you can keep this one
+website_template="index.html"
+#what the bot replies, if empty message is send (auto translated to target language)
+empty_input_prompt="System: Please enter your message."
