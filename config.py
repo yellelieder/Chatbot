74a6a5235834efba3244d63767cd84aaa842a9e2
yellelieder <55919613+yellelieder@users.noreply.github.com>
2022-05-02 12:40:33 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 #https://www.deepl.com/en/docs-api/
-deepl_api_key = "c6221dd2-1ea6-09c1-a17c-c943aa592ea6:fx"
+deepl_api_key = ""
 #https://openai.com/api/
-openai_api_key = "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"
+openai_api_key = ""
 #bot output language as in https://www.deepl.com/docs-api/other-functions/listing-supported-languages/
 target_language="DE"
 #you can keep this one
